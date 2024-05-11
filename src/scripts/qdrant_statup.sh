6a8e64c37200d3df090d5382e01d379fb90a0339
Oliver Wilkins <okwilkins@googlemail.com>
2024-05-11 21:46:06 +0100

    feat: Add startup script for Qdrant service and create collection

---
+++ src/scripts/qdrant_statup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# qdrant_startup.sh
+
+./qdrant > logs.log & sleep 1
+
+while ! curl -H "Accept: application/json" --connect-timeout 2 -s -D - "http://localhost:6333" -o /dev/null 2>/dev/null | head -n1 | grep 200 > /dev/null; do
+  echo "Qdrant is not up yet. Waiting for 1 second."
+  sleep 1
+done
+
+echo "Qdrant is up and running! Creating a collection."
+curl -s -X PUT 'http://localhost:6333/collections/nomic-embed-text-v1.5' \
+  -H 'Content-Type: application/json' \
+  --data-raw '{
+    "vectors": {
+      "size": 768,
+      "distance": "Dot"
+    }
+  }'
+
+tail -f logs.log
