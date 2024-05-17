fe4166177b429e1e9d696ec308ca379179a1cd74
Oliver Wilkins <okwilkins@googlemail.com>
2024-05-17 17:34:55 +0000

    feat: Update run_embedder function to handle both text and file inputs

--- src/app/main.py
+++ src/app/main.py
@@ -3,7 +3,7 @@ import logging
 import re
 import sys
 import json
-from typing import Optional
+from typing import Optional, TextIO
 import uuid
 from ollama import Client
 from qdrant_client import QdrantClient
@@ -21,7 +21,6 @@ def list_of_floats(arg: str) -> list[float]:
             arg = match.group(1)
         else:
             raise argparse.ArgumentTypeError("List must be a list of floats in the form [0.1, 0.2, ...]")
-
         return [float(x.strip()) for x in arg.split(",")]
     except ValueError:
         raise argparse.ArgumentTypeError("List must be a list of floats in the form [0.1, 0.2, ...]")
@@ -75,16 +74,17 @@ def cli() -> argparse.Namespace:
     )
 
     vector_store_parser.add_argument(
-        "embeddings",
-        help="Embeddings to store. Must be a list of floats in the form [0.1, 0.2, ...]",
-        type=list_of_floats,
-        nargs="+",
+        "embedding",
+        help="File with embedding to store. Must be a list of floats in the form [0.1, 0.2, ...] (default: stdin)",
+        type=argparse.FileType('r'),
+        nargs="?",
+        default=sys.stdin,
     )
 
     return parser.parse_args()
 
 
-def run_embedder(ollama_url: str, text: Optional[str] = None, file: Optional[str] = None):
+def run_embedder(ollama_url: str, text: Optional[str] = None, file: Optional[TextIO] = None):
     """Runs the embedder."""
     if text is None and file is None:
         raise ValueError("Either text or file must be provided.")
@@ -105,22 +105,20 @@ def run_embedder(ollama_url: str, text: Optional[str] = None, file: Optional[str
     logger.info(f"Embedding text")
 
     if text is None and file is not None:
-        with open(file, "r") as f:
-            text = f.read()
+        text = file.read().strip()
 
     embeddings = client.embeddings(
         model="nomic-embed-text:v1.5",
         prompt=text,
     )
     embeddings["text"] = text
-        
+
     logger.info("Text embedded")
 
-    sys.stdout.write(json.dumps(embeddings)
-)
-    
+    sys.stdout.write(json.dumps(embeddings))
+   
 
-def run_vector_store(qdrant_url: str, collection_name: str, embeddings: list[list[float]]):
+def run_vector_store(qdrant_url: str, collection_name: str, embedding: list[float]):
     """Store embeddings in the vector store."""
     logging.basicConfig(level=logging.INFO)
     handler = logging.StreamHandler(sys.stderr)
@@ -136,17 +134,13 @@ def run_vector_store(qdrant_url: str, collection_name: str, embeddings: list[lis
     logger.info("Connected to vector database")
 
     logger.info(f"Upserting vector")
-    points = []
+    point = PointStruct(
+        id=uuid.uuid4().hex,
+        vector=embedding,
+        # payload=article.as_dict(),
+    )
 
-    for embedding in embeddings:
-        point = PointStruct(
-            id=uuid.uuid4().hex,
-            vector=embedding,
-            # payload=article.as_dict(),
-        )
-        points.append(point)
-
-    client.upsert(collection_name=collection_name, points=points)
+    client.upsert(collection_name=collection_name, points=[point])
     logger.info("Vector upserted")
 
 
@@ -156,21 +150,13 @@ def main():
     if args.command in ["embed"]:
         # If the file argument is not provided, read from stdin
         if args.file == sys.stdin:
-            run_embedder(
-                text=sys.stdin.read(),
-                ollama_url=args.ollama_url,
-            )
+            run_embedder(text=sys.stdin.read(), ollama_url=args.ollama_url)
         else:
-            run_embedder(
-                file=args.file,
-                ollama_url=args.ollama_url,
-            )
+            run_embedder(file=args.file, ollama_url=args.ollama_url)
     elif args.command in ["vector-store"]:
-        run_vector_store(
-            qdrant_url=args.qdrant_url,
-            collection_name=args.collection_name,
-            embeddings=args.embeddings,
-        )
+        embedding_input = args.embedding.read().strip()
+        embedding = list_of_floats(embedding_input)
+        run_vector_store(qdrant_url=args.qdrant_url, collection_name=args.collection_name, embedding=embedding)
 
 
 if __name__ == "__main__":
