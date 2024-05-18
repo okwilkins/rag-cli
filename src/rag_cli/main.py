f131eee11c5df0bd334096eabe33ea70b730a09b
Oliver Wilkins <okwilkins@googlemail.com>
2024-05-18 19:48:18 +0100

    refactor: Update import statement in main.py

--- src/rag_cli/main.py
+++ src/rag_cli/main.py
@@ -1,7 +1,7 @@
 import sys
 from rag_cli.embedder import run_embedder
 from rag_cli.vector_store import run_vector_store
-from cli import cli, list_of_floats
+from rag_cli.cli import cli, list_of_floats
 
 
 def main():
