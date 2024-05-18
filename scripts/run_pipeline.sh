2abb3c203b21be8382cc38dce013e3dad4586b63
Oliver Wilkins <okwilkins@googlemail.com>
2024-05-18 20:17:31 +0100

    refactor: Update run_pipeline.sh to use 'rag-cli' instead of 'python main.py'

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -1,28 +1,18 @@
 #! /bin/bash
 # run_pipeline.sh
 
-# Pick the correct Python command
-if command -v python3 &> /dev/null; then
-    PYTHON=python3
-elif command -v python &> /dev/null; then
-    PYTHON=python
-else
-    echo "Python not found"
-    exit 1
-fi
-
 wikipedia_data=$(curl -L -s "https://en.wikipedia.org/api/rest_v1/page/random/summary")
 text_to_embed=$(jq -r ".title, .description, .extract" <(echo $wikipedia_data))
 
 embedding_data=$(
     echo $text_to_embed | \
-    $PYTHON ./src/app/main.py embed --ollama-url localhost:11434 2>> output.log
+    rag-cli ./src/app/main.py embed --ollama-url localhost:11434 2>> output.log
 )
 
 payload_data=$(jq "{title: .title, description: .description, extract: .extract}" <(echo $wikipedia_data))
 
 echo $(jq -r ".embedding" <(echo $embedding_data)) | \
-$PYTHON ./src/app/main.py vector-store \
+    rag-cli ./src/app/main.py vector-store \
     --qdrant-url http://localhost:6333 \
     --collection-name nomic-embed-text-v1.5 \
     --data "$payload_data" \
