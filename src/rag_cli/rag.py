b85d32e1817f93d3384fb409216c9e76f689972b
Oliver Wilkins <okwilkins@googlemail.com>
2024-05-31 20:46:55 +0100

    feat: Inject similar points into prompt and generate response

--- src/rag_cli/rag.py
+++ src/rag_cli/rag.py
@@ -43,13 +43,37 @@ def run_rag(
     similar_points = qdrant_client.search(
         collection_name=collection_name,
         query_vector=query_embedding["embedding"],
-        search_params=models.SearchParams(exact=False),
+        search_params=models.SearchParams(exact=True),
         score_threshold=min_similarity,
         limit=top_k,
     )
 
-    for point in similar_points:
-        print(point)
+    prompt_injection = '\n'.join([
+        f"Title: {similar_point.payload['title']}\n{similar_point.payload['extract']}\n"
+        for similar_point in similar_points
+    ])
+
+    logger.info("Injecting similar points into the prompt and generating the response")
+    prompt_intro = (
+        "You are a helpful AI assistant. You will be provided a prompt and information associated with it. "
+        "You need to provide the answer to the question in the prompt.\n"
+        "Associated information:\n\n"
+    )
+    prompt = f"{prompt_intro}\n\n{prompt_injection}"
+    response = ollama_client.chat(
+        model="phi3:3.8b",
+        messages=[
+            {
+                "role": "system",
+                "content": prompt,
+            },
+            {
+                "role": "user",
+                "content": query,
+            }
+        ],
+    )
+    print(response["message"]["content"])
 
 
 if __name__ == "__main__":
@@ -58,6 +82,7 @@ if __name__ == "__main__":
         qdrant_url="http://localhost:6333",
         collection_name="nomic-embed-text-v1.5",
         top_k=5,
-        min_similarity=0.5,
-        query="Shiveegovi",
+        min_similarity=0.2,
+        query="What engines did Nissan make?",
     )
+ 
